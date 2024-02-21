e37dfce0825940a9fa46078f7360920d377f515c
Aayush <therespawner69@gmail.com>
2024-02-21 21:27:09 +0545

    some more unsaved models

--- fundapp/models.py
+++ fundapp/models.py
@@ -12,3 +12,45 @@ class AllFund(models.Model):
     
     def __str__(self):
         return f'{self.title} --by {self.user}'
+    
+    
+class EsewaClaim(models.Model):
+    sno = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='esewa_claim_user')
+    phoneNumber = models.CharField(max_length=15)
+    
+    def __str__(self):
+        return f'Caimed by -- {self.user}'
+    
+    
+class KhaltiClaim(models.Model):
+    sno = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='khalti_claim_user')
+    phoneNumber = models.CharField(max_length=15)
+    
+    def __str__(self):
+        return f'Caimed by -- {self.user}'
+    
+    
+class BankClaim(models.Model):
+    sno = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bank_claim_user')
+    bankName = models.CharField(max_length=180)
+    accountNumber = models.CharField(max_length = 150)
+    ReceivingName = models.CharField(max_length = 100)
+    # phoneNumber (sai confirmation)
+    
+    def __str__(self):
+        return f'Caimed by -- {self.user}'
+    
+class Transaction(models.Model):
+    sno = models.AutoField(primary_key=True)
+    amountDonator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='transaction_user_donator')
+    medium = models.CharField(max_length=50)
+    amount = models.DecimalField(max_digits=19, decimal_places=3)
+    amountReceiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='transaction_user_receiver')
+    campaignTitle = models.ForeignKey(AllFund, on_delete=models.CASCADE, related_name = 'transaction_campaign_title')
+    # Address (sai confirmation)
+    
+    def __str__(self):
+        return f'Transaction of -- {self.user}'
